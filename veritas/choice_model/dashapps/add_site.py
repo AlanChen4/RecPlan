d2e1bc5a63ad2552e996ea2bc8419d81f9344d97
Alan Chen <avchen4@gmail.com>
2022-04-21 14:33:03 -0400

    Add plotly-dash graph for adding a new site

--- veritas/choice_model/dashapps/add_site.py
+++ veritas/choice_model/dashapps/add_site.py
@@ -10,3 +10,11 @@ app = DjangoDash('AddSite')
 app.layout = html.Div(children=[
     dcc.Graph(id='add-site-plot', figure=None)
 ])
+
+@app.callback(
+    Output('add-site-plot', 'figure'),
+    [Input('add-site-plot', 'clickData')])
+def update_figure(clickData):
+    if clickData is not None:
+        geo_id = clickData['points'][0]['location']
+        print(geo_id)
