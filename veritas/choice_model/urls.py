1b03256fb9154565462c38257bfa8f6bc8a086d5
Alan Chen <avchen4@gmail.com>
2022-04-27 21:32:45 -0400

    Update urls to more appropriate names

--- veritas/choice_model/urls.py
+++ veritas/choice_model/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', BundleList.as_view(), name='bundles'),
     path('bundles/', BundleList.as_view(), name='bundles'),
     path('bundle/<uuid:pk>/', BundleDetail.as_view(), name='bundle'),
-    path('bundle-create/', BundleCreate.as_view(), name='bundle-create'),
-    path('bundle-update/<uuid:pk>/', BundleUpdate.as_view(), name='bundle-update'),
-    path('bundle-delete/<uuid:pk>/', BundleDelete.as_view(), name='bundle-delete'),
+    path('bundle/', BundleCreate.as_view(), name='bundle-create'),
+    path('bundle/<uuid:pk>/update/', BundleUpdate.as_view(), name='bundle-update'),
+    path('bundle/<uuid:pk>/delete/', BundleDelete.as_view(), name='bundle-delete'),
 
-    path('modified-site-create/<uuid:pk>/<str:site_name>/', ModifiedSiteCreate.as_view(), name='modified-site-create'),
+    path('modified-site/<uuid:pk>/<str:site_name>/', ModifiedSiteCreate.as_view(), name='modified-site-create'),
 ]
