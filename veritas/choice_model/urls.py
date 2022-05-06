48597b7c35d1f30fec1920f3f8b6f34b11bbe721
Alan Chen <avchen4@gmail.com>
2022-05-05 22:05:28 -0400

    Change 'bundle' in URL to 'model' to match front end references to bundle model

--- veritas/choice_model/urls.py
+++ veritas/choice_model/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', BundleList.as_view(), name='bundles'),
-    path('bundles/', BundleList.as_view(), name='bundles'),
-    path('bundle/<uuid:pk>/', BundleDetail.as_view(), name='bundle'),
-    path('bundle/', BundleCreate.as_view(), name='bundle-create'),
-    path('bundle/<uuid:pk>/update/', BundleUpdate.as_view(), name='bundle-update'),
-    path('bundle/<uuid:pk>/delete/', BundleDelete.as_view(), name='bundle-delete'),
+    path('models/', BundleList.as_view(), name='bundles'),
+    path('model/<uuid:pk>/', BundleDetail.as_view(), name='bundle'),
+    path('model/', BundleCreate.as_view(), name='bundle-create'),
+    path('model/<uuid:pk>/update/', BundleUpdate.as_view(), name='bundle-update'),
+    path('model/<uuid:pk>/delete/', BundleDelete.as_view(), name='bundle-delete'),
 
     path('modified-site/<uuid:pk>/<str:site_name>/', ModifiedSiteCreate.as_view(), name='modified-site-create'),
 ]
