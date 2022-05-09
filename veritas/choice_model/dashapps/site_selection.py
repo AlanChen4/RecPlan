c5cbac07370e43e0ac6630683dff2fe3a6f61262
Alan Chen <avchen4@gmail.com>
2022-05-09 16:07:34 -0400

    Update import styling

--- veritas/choice_model/dashapps/site_selection.py
+++ veritas/choice_model/dashapps/site_selection.py
@@ -1,6 +1,4 @@
-import dash_core_components as dcc
-import dash_html_components as html
-
+from dash import dcc, html
 from dash.dependencies import Input, Output
 from django_plotly_dash import DjangoDash
 
