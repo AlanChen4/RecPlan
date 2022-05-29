8281ad317db614237f052e8205a1f58c465cde42
Alan Chen <avchen4@gmail.com>
2022-05-29 18:06:27 -0400

    Set debug to false for production

--- veritas/veritas/settings.py
+++ veritas/veritas/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6%g*&mbwg()+l%u72xdut&zi7lxbon&)7na3ckjpn2(a*-7gq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q6%g*&mbwg()+l%u72xdut&zi7lxbon&)7na3ckjpn2(a*-7gq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
