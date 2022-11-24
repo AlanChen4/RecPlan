8a00406b0c0d154617d0ac82f5a99fbbef905605
Alan Chen <avchen4@gmail.com>
2022-11-23 23:02:16 -0500

    Fix: Upgrade joblib to protect against vulnerability

--- veritas/authentication/urls.py
+++ veritas/authentication/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
-    path('landing/', LandingPageView.as_view(), name='landing_page'),
+    path('', LandingPageView.as_view(), name='landing_page'),
     path('profile/<uuid:pk>/', UserUpdateView.as_view(), name='profile'),
 ]
